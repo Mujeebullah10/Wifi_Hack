1ba6c6043b773562014197e57ab02c0db9824a27
Mujeebullah10 <Mujeebullah2121@gmail.com>
2025-09-12 19:11:08 +0500

    Update birihack.py

--- birihack.py
+++ birihack.py
@@ -22,7 +22,7 @@ print('''
 BiRi_B@B@
 Telegram:https://t.me/termux_hacker_bd
 One line Command:\nsudo python Wifi_Hack/birihack.py -i wlan0 -K\n
-For Help Type:\npython Wifi_Hack/birihack.py --help\033[0m
+For Help Type:\npython Wifi_Hack/birihack.py --mr
 ''')
 class NetworkAddress:
     def __init__(self, mac):
